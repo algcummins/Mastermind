9be2e27e3b944c836e489a3de8c3a95d9aa5cc9c
Anthony Cummins <39380486+algcummins@users.noreply.github.com>
2018-05-18 18:37:20 +0200

    Removed global variables

--- mastertools.py
+++ mastertools.py
@@ -11,14 +11,12 @@ import random
 def gencode():
     codeint = random.sample(range(1,6),4) #selects 4 different numbers
     codestr = [str(i) for i in codeint]
-    global code 
     code = ''.join(codestr)
  #   print(code)
     return code
 
 
 def goodplace(code, guess):
-    global placetotal
     placetotal = 0
     for i in range(4):
         if guess[i] == code[i]:
@@ -31,7 +29,7 @@ def goodcolor(code, guess):
     for color in guess:
             if color in code:
                 temptotal += 1
-    colortotal = temptotal - placetotal
+    colortotal = temptotal - goodplace(code, guess): 
     return colortotal 
 
 
